8b039952bafd9ae7573ad73b27cd215d1dfbacd9
Aniket Sedhai <aniketsedhai@gmail.com>
2024-10-05 18:23:19 -0500

    finished the assignment

---
+++ finder-app/writer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "enter 2 arguments (writefile, writestr)"
+    exit 1
+fi
+
+mkdir -p $(dirname $1) && touch $1
+echo $2 > $1
+
+if [ $? -ne 0 ]; then
+    echo "file creation failed"
+    exit 1
+fi
